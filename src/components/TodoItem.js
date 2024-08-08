2e3ca7ea14be110a9fcdb7d63b7fa6a1354be30b
christyaurelia9@gmail.com <christyaurelia9@gmail.com>
2024-08-08 14:18:52 +0700

    my changes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,23 +1,31 @@
 const TodoItem = ({ todo, setRefresh }) => {
-    const updateTodo = () => {
-      todo.done = !todo.done;
-      fetch("http://localhost:8000/todos/" + todo.id, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(todo)
-      }).then(() => {
-        console.log('Todo updated.');
-        setRefresh(true);
-      });
-    };
-  
-    return (
-      <li className={`${todo.done ? "checked" : ""}`}>
-        <div onClick={updateTodo}>{todo.title}</div>
-        <span className="close">x</span>
-      </li>
-    );
+  const updateTodo = () => {
+    todo.done = !todo.done;
+    fetch("http://localhost:8000/todos/" + todo.id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(todo)
+    }).then(() => { 
+      console.log('Todo updated.');
+      setRefresh(true);
+    });
   };
-  
-  export default TodoItem;
-  
+
+  const deleteTodo = () => {
+    fetch("http://localhost:8000/todos/" + todo.id, {
+      method: "DELETE",
+    }).then(() => {
+      console.log('Todo deleted.');
+      setRefresh(true);
+    });
+  };
+
+  return (
+    <li className={`${todo.done ? "checked" : ""}`}>
+      <div onClick={updateTodo}>{todo.title}</div>
+      <span className="close" onClick={deleteTodo}>x</span>
+    </li>
+  );
+};
+
+export default TodoItem;
