e3cd47e3571a89e4a91b4f7f1d531103ce6a2ae5
christyaurelia9@gmail.com <christyaurelia9@gmail.com>
2024-08-08 14:02:34 +0700

    Tugas Pak jaka To Do App

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+const TodoItem = ({ todo, setRefresh }) => {
+    const updateTodo = () => {
+      todo.done = !todo.done;
+      fetch("http://localhost:8000/todos/" + todo.id, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(todo)
+      }).then(() => {
+        console.log('Todo updated.');
+        setRefresh(true);
+      });
+    };
+  
+    return (
+      <li className={`${todo.done ? "checked" : ""}`}>
+        <div onClick={updateTodo}>{todo.title}</div>
+        <span className="close">x</span>
+      </li>
+    );
+  };
+  
+  export default TodoItem;
+  
